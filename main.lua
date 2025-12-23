0d5929701fa4b0779c32bdaf65c141d83168c539
Taiko Elliwaki <taikoelliwaki@gmail.com>
2025-12-22 16:55:11 -0800

    balls

--- main.lua
+++ main.lua
@@ -6,7 +6,14 @@ character.x = 300
 character.y = 400
 
 
-pop = love.graphics.newImage ("kirb.png")
+character2 = {}
+character2.x = 300
+character2.y = 400
+
+
+pop  = love.graphics.newImage ("1.png")
+
+pop2 = love.graphics.newImage ("2.png")
 
 end
 
@@ -14,6 +21,7 @@ function love.draw()
 
 love.graphics.draw(pop, character.x, character.y)
 
+love.graphics.draw(pop2, character2.x, character2.y)
 
 end
 
@@ -41,4 +49,33 @@ function love.update(dt)
     character.x = character.x - 1
 
   end
+
+--
+--
+--
+  if love.keyboard.isDown('h') then
+
+    character2.y = character2.y + 1
+
+  end
+     if love.keyboard.isDown('j') then
+
+    character2.x = character2.x + 1
+
+  end
+  if love.keyboard.isDown('y') then
+
+    character2.y = character2.y - 1
+
+  end
+  if love.keyboard.isDown('g') then
+
+
+    character2.x = character2.x - 1
+
+  end
+
+
 end
+
+
